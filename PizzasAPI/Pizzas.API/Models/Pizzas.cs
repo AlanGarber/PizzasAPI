9713b1b4e9edfaf04f52909874259474f179dfe4
AlanGarbur15 <alangarber1@gmail.com>
2022-03-22 22:57:54 -0300

    Piiza constructor

--- PizzasAPI/Pizzas.API/Models/Pizzas.cs
+++ PizzasAPI/Pizzas.API/Models/Pizzas.cs
@@ -10,4 +10,13 @@ namespace Pizzas.API.Models
         public float    Importe         { get; set; }
         public string   Descripcion     { get; set; }
     }
+
+    public Pizza(int Id ,string Nombre, bool LibreGluten, float Importe, string Descripcion){
+            _IdPizza=Id;
+            _nombre=Nombre;
+            _LibreGluten=LibreGluten;
+            _Importe=Importe;
+            _descripcion=Descripcion;
+        }
+
 }
