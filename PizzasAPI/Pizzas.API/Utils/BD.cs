d5d448cfe5d3a8177482ff22958a004f02d86860
ALAN DAVID <46442525@ort>
2022-03-30 10:33:18 -0300

    30/3

---
+++ PizzasAPI/Pizzas.API/Utils/BD.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pizzas.API.Models;
+using System.Data.SqlClient;
+using Dapper;
+
+using Pizzas.API.Services.ConfigurationHelper;
+
+namespace Pizzas.API.Utils
+{
+    public static class BD
+    {
+        public static SqlConnection GetConnection(){
+            SqlConnection db;
+            string connectionString;
+
+            connectionString=ConfigurationHelper.GetConfiguration().GetValue<string>("DatabaseSettings:ConnectionString");
+            db= new SqlConnection(connectionString);
+            return db;
+        }
+    }
+}
